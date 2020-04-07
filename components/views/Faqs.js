051c3230e429ce390df90a4a0b1a312ea698e2b9
Ashley E. Whitehouse <a.whitehouse@callcpc.com>
2020-04-07 16:48:15 -0500

    addedpics

--- components/views/Faqs.js
+++ components/views/Faqs.js
@@ -1,14 +1,14 @@
 export default () => `<div class="faq_container">
     <div class="faq">
-        <div class="faq_question" id="q1">Why did you create such a platform? +</div>
+        <div class="faq_question" id="q1">+ Why did you create such a platform?</div>
         <div class="faq_answer hidden" id="a1">Because this industry has been through enough forceful hoops that it was time that they were able to have a platform as stress-free as possible</div>
-        <div class="faq_question" id="q2">What does one need to sign up? +</div>
+        <div class="faq_question" id="q2">+ What does one need to sign up?</div>
         <div class="faq_answer hidden" id="a2">You will need an email and an account on the banking app we use.</div>
-        <div class="faq_question" id="q3">When I sign up, do I have to sell or buy things? +</div>
+        <div class="faq_question" id="q3">+ When I sign up, do I have to sell or buy things?</div>
         <div class="faq_answer hidden" id="a3">No, it is not mandatory to do either</div>
-        <div class="faq_question" id="q4">Why do you require one banking app? +</div>
+        <div class="faq_question" id="q4">+ Why do you require one banking app?</div>
         <div class="faq_answer hidden" id="a4">To make it easier for not only us but vendor and buyer</div>
-        <div class="faq_question" id="q5">Are you only able to view knives and guns? +</div>
+        <div class="faq_question" id="q5">+ Are you only able to view knives and guns?</div>
         <div class="faq_answer hidden" id="a5">For right now, yes. This could change in the future</div>					
     </div>
 </div>`;
