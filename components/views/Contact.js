78210d3b5475edc2130c6b8deaafad612d6ca793
Ashley E. Whitehouse <a.whitehouse@callcpc.com>
2020-04-07 21:57:31 -0500

    debugfix

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,15 +1,18 @@
 export default () => `<h2><center>Please feel free to reach out with any questions and/or concerns through the following formats</center></h2>
     <div class="contact">
         <div class="alternate">
-            <div><h3>Fill out and submit this form</h3><br>
+            <h3>Fill out and submit this form</h3><br>
             <form action="https://formspree.io/mdodqbdd" method="POST">
                 <label>Name:<input type="text" name="_replyto"></label><br>
                 <label>Email:<input type="text" name="_replyto"></label><br>
                 <label>Message:<textarea name="message"></textarea></label><br>
                 <button type="submit">Send</button>
-                </form><br></div>
-        <div><h3>EMAIL:</h3><br>
-            <h4>info@allinone.com</h4><br></div></div>
+                </form><br>
+        </div>
+        <div class="alternate">
+            <div><h3>EMAIL:</h3><br>
+                <h4>info@allinone.com</h4><br></div>
+        </div>
         <div><h3>Address & Phone Number</h3><br>
             <h4>555 W Hollywood Dr<br>Granite City, SD 54857<br>555-667-1234</h4><br></div>
         <div><h3>Social Media</h3><br></div>
